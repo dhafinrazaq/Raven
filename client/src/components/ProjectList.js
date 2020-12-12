17774f93302e9107c141b847c0bdeb6322843fe8
dhafin <dhafinrazaq@gmail.com>
2020-12-12 20:16:13 +0800

    Edit project list page to horizontal list

--- client/src/components/ProjectList.js
+++ client/src/components/ProjectList.js
@@ -19,21 +19,34 @@ export class ProjectList extends Component {
 
     return (
       <Container>
-        <ListGroup horizontal>
+        <ul className="project-list">
           {projects.map(({ _id, name }) => (
-            <ListGroupItem>
-              <Button
+            <li className="project-list-item">
+              {/* <Button
                 className="remove-btn"
                 color="danger"
                 size="sm"
                 onClick={this.onDeleteClick.bind(this, _id)}
               >
                 x
-              </Button>
-              {name}
-            </ListGroupItem>
+              </Button> */}
+              <figure class="figure">
+                <div class="row">
+                  <div class="col-md-8 offset-md-2">
+                    <img
+                      src="https://via.placeholder.com/300.png/09f/fff"
+                      class="figure-img img-fluid mx-auto"
+                      alt="A generic square placeholder image with rounded corners in a figure."
+                      style={{ maxHeight: "100%", maxWidth: "100%" }}
+                    ></img>
+                  </div>
+                </div>
+
+                <div class="text-center">{name}</div>
+              </figure>
+            </li>
           ))}
-        </ListGroup>
+        </ul>
       </Container>
     );
   }

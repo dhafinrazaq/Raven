ae616ea05a02b5264b00959d2308fa14f52554de
FH-30 <finxictus@gmail.com>
2020-12-16 03:50:20 +0800

    Add comments

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,9 +5,19 @@ const router = express.Router();
 // Get all user controllers that manipulate the user database
 const userControllers = require("../../controllers/userControllers");
 
+// @route POST api/users/signup
+// @desc add new user to database and authenticate user
+// @access public
 router.post("/signup", userControllers.signUpController);
+
+// @route POST api/users/signin
+// @desc authenticate user based on given credentials
+// @access public
 router.post("/signin", userControllers.signInController);
 
+// @route GET api/users/data
+// @desc get data of the user in the current session
+// @access public
 router.get("/data", userControllers.getUserDataController);
 
 module.exports = router;
